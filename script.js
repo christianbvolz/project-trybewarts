1a964cf41aa153c61dac76963a21607a2bef746a
Christianbv <christianbvolz@gmail.com>
2021-07-21 18:42:22 -0300

    requisitos  18 e 19

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const checkBox = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
+
+checkBox.addEventListener('click', (element) => {
+  if (element.target.checked === true) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+});
