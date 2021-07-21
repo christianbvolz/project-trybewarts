e4d9c5bc0238c724ef259a403c58f482353a27e4
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-07-21 19:07:32 -0300

    arruma o lint e faz o merge

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const checkBox = document.getElementById('agreement');
 const submitBtn = document.getElementById('submit-btn');
 
@@ -9,20 +8,17 @@ checkBox.addEventListener('click', (element) => {
     submitBtn.disabled = true;
   }
 });
-=======
+
 const btnLogin = document.getElementById('login');
 
 btnLogin.addEventListener('click', (event) => {
-event.preventDefault();
-let login = document.querySelectorAll('.trybewarts-login input')[0].value
-let senha = document.querySelectorAll('.trybewarts-login input')[1].value
+  event.preventDefault();
+  const login = document.querySelectorAll('.trybewarts-login input')[0].value;
+  const senha = document.querySelectorAll('.trybewarts-login input')[1].value;
 
   if ((login === 'tryber@teste.com') && (senha === '123456')) {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
-})
-
-const checkBox = document.getElementById('agreement');
->>>>>>> 024d9056596e9616a5daca7826a7ba33ec29ce10
+});
